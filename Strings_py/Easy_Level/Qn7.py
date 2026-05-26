dcbb8cf7473a584b699dcd067843fe18bb029307
Ashish RAj <rajshish37@gmail.com>
2026-05-26 21:45:11 +0530

    String in python

---
+++ Strings_py/Easy_Level/Qn7.py
@@ -0,0 +1,12 @@
+'''1. Convert to Lowercase
+    
+    Problem: Convert all characters of `s` to lowercase.
+Input:
+s = "PYTHON"
+
+Output:
+"python"'''
+
+s = input("Enter a string: ")
+
+print(s.lower())
