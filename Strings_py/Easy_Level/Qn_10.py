9a9c7d3fb60c2b14fe35d74c79d2e36b0feb6fe7
Ashish RAj <rajshish37@gmail.com>
2026-05-26 22:04:03 +0530

    Strings in pyhton

---
+++ Strings_py/Easy_Level/Qn_10.py
@@ -0,0 +1,17 @@
+'''1. First Character
+    
+    Problem: Return first character of string `s`.
+Input:
+s = "Python"
+
+Output:
+"P"'''
+s = input("Enter a string: ")
+print(s[0])
+# first_char = " "
+
+# for i in s:
+#      first_char = i
+#      break
+
+# print(first_char)
