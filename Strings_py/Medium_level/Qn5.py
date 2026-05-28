cc76411f8d9a001ae11f3a6d6efc53a5a7a9ef1f
Ashish RAj <rajshish37@gmail.com>
2026-05-28 22:26:30 +0530

    Strings in python

---
+++ Strings_py/Medium_level/Qn5.py
@@ -0,0 +1,12 @@
+'''1. Capitalize Words
+    
+    Problem: Capitalize first letter of each word.
+Input:
+s = "hello world"
+
+Output:
+"Hello World"'''
+
+s = "hello world"
+
+print(s.capitalize())
