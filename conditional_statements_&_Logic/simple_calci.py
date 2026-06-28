13896f874042e53d597a55a8ff56b3ef22e5b6f0
Ashish RAj <rajshish37@gmail.com>
2026-06-28 21:53:32 +0530

    initial commit

--- conditional_statements_&_Logic/simple_calci.py
+++ conditional_statements_&_Logic/simple_calci.py
@@ -12,4 +12,11 @@ match op:
     case '*':
         print(num1," * ",num2, "=",(num1*num2))
     case '/':
-        print(num1," / ",num2, "=",(num1/num2))
+        if num2 != 0:
+            print(num1," + ",num2, "=",(num1+num2))
+        else:
+            print("Division by zero is not allowed ")
+    case _:
+        print("invalid operator")
+
+            
