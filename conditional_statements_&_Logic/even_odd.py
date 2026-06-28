c2b3b13e91ee2f4a5dfd8d44c2f29b365ec8f755
Ashish RAj <rajshish37@gmail.com>
2026-06-28 21:29:04 +0530

    initial commit

---
+++ conditional_statements_&_Logic/even_odd.py
@@ -0,0 +1,8 @@
+# Write a program to check whether a number is even or odd using if-else.
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
